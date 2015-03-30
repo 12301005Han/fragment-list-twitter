2b9939f7143d8b20bbbc750b77a28327919c4782
samsung <12301005@bjtu.edu.cn>
2015-03-30 14:40:15 +0800

    12301005

---
+++ app/src/main/java/com/deitel/twittersearches/WebFragment.java
@@ -0,0 +1,64 @@
+package com.deitel.twittersearches;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link WebFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class WebFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "url";
+
+    // TODO: Rename and change types of parameters
+    private String url;
+
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @return A new instance of fragment WebFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static WebFragment newInstance(String param1) {
+        WebFragment fragment = new WebFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public WebFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            url = getArguments().getString(ARG_PARAM1);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v=inflater.inflate(R.layout.fragment_web, container, false);
+        ((WebView)v.findViewById(R.id.wv)).loadUrl(url);
+        return v;
+    }
+
+
+}
